35b994e0abbe747bc78dcdae550758c4faef252f
Dinesh Arney <dinesharney@gmail.com>
2024-12-30 16:07:41 +0530

    Ignite external storage example

--- src/main/java/com/example/ignite/server/IgniteSpringBootServerApp.java
+++ src/main/java/com/example/ignite/server/IgniteSpringBootServerApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IgniteServerNodeApp {
+public class IgniteSpringBootServerApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IgniteServerNodeApp.class, args
+		SpringApplication.run(IgniteSpringBootServerApp.class, args
 		);
 	}
 }
